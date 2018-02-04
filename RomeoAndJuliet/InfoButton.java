6053320c8495b50b3ab1010c2e2681bb42b0ee60
Aofpcc <Bunyarit65@hotmail.co.th>
2018-02-04 15:12:44 +0700

    Can click button

--- RomeoAndJuliet/InfoButton.java
+++ RomeoAndJuliet/InfoButton.java
@@ -12,21 +12,32 @@ public class InfoButton extends GameUI
      * Act - do whatever the InfoButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
+    private boolean isNormal;
+    private int time;
     public InfoButton ()
     {
         animNormal = new GreenfootImage("Button/InfoButton/notclick.png");
         setImage(animNormal);
         animClick = new GreenfootImage("Button/InfoButton/holdon.png");
+        isNormal = true;
     }
     
     public void act()
     {
-        if(Greenfoot.getMouseInfo().getActor() != null ||Greenfoot.getMouseInfo().getActor() == this)
-        {
-            setImage(animClick);
+        if( !isNormal ){
+            time--;
+            if(time == 0) isNormal = true;
         }
-        else if(true)
+        else if( Greenfoot.getMouseInfo() == null ){}
+        else if(Greenfoot.getMouseInfo().getActor() != null && Greenfoot.getMouseInfo().getActor() == this)
+        {
+            if( Greenfoot.getMouseInfo().getClickCount() %2 == 1 ){
+                setImage(animClick);
+                isNormal = false;
+                time = 10;
+            }
+        }
+        else if(isNormal)
         {
             setImage(animNormal);
         }
